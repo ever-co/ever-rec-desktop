5d764c2f8d9340b0858c8b853e887259670607d8
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-07-01 16:53:30 +0200

    Refactor captureScreenEvent to use desktopCapturer and screen modules instead of BrowserWindow

--- libs/electron/event/src/lib/capture-screen.event.ts
+++ libs/electron/event/src/lib/capture-screen.event.ts
@@ -1,18 +1,25 @@
 import { channel } from '@prototype/shared/utils';
-import { BrowserWindow, ipcMain } from 'electron';
+import { desktopCapturer, ipcMain, screen } from 'electron';
+
+export function getWindowSize(): { height: number; width: number } {
+  const display = screen.getPrimaryDisplay();
+  return {
+    height: display.workAreaSize.height,
+    width: display.workAreaSize.width,
+  };
+}
 
 export function captureScreenEvent(): void {
   ipcMain.on(channel.CAPTURE_SCREEN, async (event, delay) => {
-    const window = BrowserWindow.fromWebContents(event.sender);
-    if (!window) {
-      console.debug('[captureScreenEvent]: No window');
-      return;
-    }
-    setTimeout(() => {
-      window.webContents.capturePage().then((image) => {
-        // Send the screenshot back to the renderer process
-        event.reply(channel.SCREENSHOT_CAPTURED, image.toDataURL());
+    setTimeout(async () => {
+      const sources = await desktopCapturer.getSources({
+        types: ['screen'],
+        thumbnailSize: getWindowSize(),
       });
+      const source = sources[0];
+      const image = source.thumbnail;
+      // Send the screenshot back to the renderer process
+      event.reply(channel.SCREENSHOT_CAPTURED, image.toDataURL());
     }, delay);
   });
 }
