e27d2bccf65f310443e7d220f9ed763ad6795fb5
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-07-02 22:44:40 +0200

    "Added ScreenshotComponent to LayoutComponent"

--- libs/web/convert-video/feature/src/lib/layout/layout.component.ts
+++ libs/web/convert-video/feature/src/lib/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ScreenshotComponent } from '../screenshot/screenshot.component';
 import { SettingComponent } from '../setting/setting.component';
 import { StatisticComponent } from '../statistics/statistic.component';
 import { VideoComponent } from '../video/video.component';
@@ -7,7 +8,13 @@ import { VideoComponent } from '../video/video.component';
 @Component({
   selector: 'lib-layout',
   standalone: true,
-  imports: [CommonModule, SettingComponent, VideoComponent, StatisticComponent],
+  imports: [
+    CommonModule,
+    SettingComponent,
+    VideoComponent,
+    StatisticComponent,
+    ScreenshotComponent,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
---
+++ libs/web/convert-video/feature/src/lib/screenshot/screenshot.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-screenshot',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './screenshot.component.html',
+  styleUrl: './screenshot.component.css',
+})
+export class ScreenshotComponent {}
