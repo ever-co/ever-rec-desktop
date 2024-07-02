9b1456bf47adf737e3ff5a0b936321d8f18f7bdb
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-07-02 22:34:14 +0200

    Added StatisticComponent to LayoutComponent, updated layout HTML and SCSS, and renamed Convert button to Generate in SettingComponent

--- libs/web/convert-video/feature/src/lib/layout/layout.component.ts
+++ libs/web/convert-video/feature/src/lib/layout/layout.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SettingComponent } from '../setting/setting.component';
+import { StatisticComponent } from '../statistics/statistic.component';
 import { VideoComponent } from '../video/video.component';
 
 @Component({
   selector: 'lib-layout',
   standalone: true,
-  imports: [CommonModule, SettingComponent, VideoComponent],
+  imports: [CommonModule, SettingComponent, VideoComponent, StatisticComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
---
+++ libs/web/convert-video/feature/src/lib/statistics/statistic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-statistic',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './statistic.component.html',
+  styleUrl: './statistic.component.scss',
+})
+export class StatisticComponent {}
