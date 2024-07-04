27a1384742dbfa2b25343fc63e78344a434c9c85
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-07-04 18:40:25 +0200

    Added 'journal_mode=WAL' pragma to prepareDatabase function in appDataSource

--- libs/electron/database/src/lib/data-source.ts
+++ libs/electron/database/src/lib/data-source.ts
@@ -14,6 +14,7 @@ export const appDataSource = new DataSource({
   migrations: [],
   prepareDatabase: (db) => {
     db.pragma(`cipher='sqlcipher'`);
+    db.pragma(`journal_mode=WAL`);
   },
 });
 
