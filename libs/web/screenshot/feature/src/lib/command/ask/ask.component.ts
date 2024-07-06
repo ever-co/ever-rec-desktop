9135e7ae14be969a69744d6136ee12e5307b1e6f
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-07-06 07:20:11 +0200

    Refactor AskComponent: remove form group, add search icon, and update styles

--- libs/web/screenshot/feature/src/lib/command/ask/ask.component.ts
+++ libs/web/screenshot/feature/src/lib/command/ask/ask.component.ts
@@ -1,36 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { screenshotActions } from '@prototype/web/screenshot/data-access';
 
 @Component({
   selector: 'lib-ask',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ask.component.html',
   styleUrl: './ask.component.scss',
 })
-export class AskComponent implements OnInit {
-  public askForm!: FormGroup;
+export class AskComponent {
+  public request!: string;
   private store = inject(Store);
-  ngOnInit(): void {
-    this.askForm = new FormGroup({
-      request: new FormControl('', Validators.required),
-    });
-  }
 
-  onSubmit() {
-    if (this.askForm.valid) {
-      this.store.dispatch(screenshotActions.ask(this.askForm.value));
-      this.askForm.reset();
-    } else {
-      this.askForm.markAllAsTouched();
-    }
+  onChange() {
+    this.store.dispatch(screenshotActions.ask({ request: this.request }));
   }
 }
