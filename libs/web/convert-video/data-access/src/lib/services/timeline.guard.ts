9c78ea6956cd89927fab62b1a64e1deb47623c3b
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-07-23 16:36:48 +0200

    refactor: timeline guard to include dependency checks, error handling, and logging

--- libs/web/convert-video/data-access/src/lib/services/timeline.guard.ts
+++ libs/web/convert-video/data-access/src/lib/services/timeline.guard.ts
@@ -2,20 +2,35 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { selectScreenshotState } from '@prototype/web/screenshot/data-access';
-import { map } from 'rxjs';
+import { catchError, map, of } from 'rxjs';
 
+/**
+ * A guard function that checks if the user is on the timeline page.
+ *
+ * @return {CanActivateFn} A function that returns an observable indicating if the user can activate the route.
+ * @throws {Error} If the store or router dependencies are not provided.
+ */
 export const timelineGuard: CanActivateFn = () => {
   const store = inject(Store);
   const router = inject(Router);
 
+  if (!store || !router) {
+    throw new Error('Store and Router are required dependencies');
+  }
+
   return store.select(selectScreenshotState).pipe(
     map(({ screenshots }) => {
-      if (screenshots.length > 0) {
-        return true;
-      } else {
+      if (!screenshots) {
         router.navigate(['/']);
         return false;
+      } else {
+        return true;
       }
+    }),
+    catchError((error) => {
+      console.error('Error in timelineGuard', error);
+      router.navigate(['/']);
+      return of(false);
     })
   );
 };
