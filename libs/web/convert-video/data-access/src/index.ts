55c67ada234028aa6c7559d3ded06c5bc144bcbf
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-07-23 16:38:34 +0200

    rename 'convert-video-access-provider.moddule' to 'convert-video-access-provider.module' and delete the file 'convert-video-access-provider.moddule.ts'

--- libs/web/convert-video/data-access/src/index.ts
+++ libs/web/convert-video/data-access/src/index.ts
@@ -8,7 +8,7 @@ export * from './lib/+state/video-remote-control/video-remote-control.selectors'
 export * from './lib/+state/video/generate-video.actions';
 export * from './lib/+state/video/generate-video.effects';
 export * from './lib/+state/video/generate-video.selectors';
-export * from './lib/convert-video-access-provider.moddule';
+export * from './lib/convert-video-access-provider.module';
 export * from './lib/services/convert-video-electron.service';
 export * from './lib/services/timeline.guard';
 export * from './test-setup';
