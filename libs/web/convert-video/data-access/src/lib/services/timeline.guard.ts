b6ef27ade7508e3cbca58d7092201d59d20b528c
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-07-24 12:23:37 +0200

    fix: timeline guard to include additional state checks and actions

--- libs/web/convert-video/data-access/src/lib/services/timeline.guard.ts
+++ libs/web/convert-video/data-access/src/lib/services/timeline.guard.ts
@@ -2,33 +2,49 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { selectScreenshotState } from '@prototype/web/screenshot/data-access';
-import { catchError, map, of } from 'rxjs';
+import { catchError, map, of, switchMap, withLatestFrom } from 'rxjs';
+import { selectSettingState } from '../+state/settings/setting.selectors';
+import { generateVideoActions } from '../+state/video/generate-video.actions';
+import { selectGenerateVideoState } from '../+state/video/generate-video.selectors';
 
 /**
  * A guard function that checks if the user is on the timeline page.
  *
  * @return {CanActivateFn} A function that returns an observable indicating if the user can activate the route.
- * @throws {Error} If the store or router dependencies are not provided.
  */
 export const timelineGuard: CanActivateFn = () => {
   const store = inject(Store);
   const router = inject(Router);
 
-  if (!store || !router) {
-    throw new Error('Store and Router are required dependencies');
-  }
-
   return store.select(selectScreenshotState).pipe(
-    map(({ screenshots }) => {
+    withLatestFrom(store.select(selectGenerateVideoState)),
+    switchMap(([screenshotState, videoState]) => {
+      const { screenshots } = screenshotState;
+      const { video } = videoState;
+
       if (!screenshots.length) {
-        router.navigate(['/']);
-        return false;
-      } else {
-        return true;
+        router.navigate(['/convert']);
+        return of(false);
       }
+
+      if (!video.pathname) {
+        return store.select(selectSettingState).pipe(
+          map(({ videoConfig }) => {
+            store.dispatch(
+              generateVideoActions.start({
+                screenshotIds: screenshots.map(({ id }) => id),
+                config: videoConfig,
+              })
+            );
+            return true;
+          })
+        );
+      }
+
+      return of(true);
     }),
     catchError((error) => {
-      console.error('Error in timelineGuard', error);
+      console.error('Error in timelineGuard:', error);
       router.navigate(['/']);
       return of(false);
     })
