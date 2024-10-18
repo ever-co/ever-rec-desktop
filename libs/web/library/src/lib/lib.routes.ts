0e8607b554e90a8187c7f6a3a01184207b8ec628
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-18 21:20:33 +0200

    "Added 'screenshots' route with GalleryComponent to library routes and updated library component HTML"

--- libs/web/library/src/lib/lib.routes.ts
+++ libs/web/library/src/lib/lib.routes.ts
@@ -4,6 +4,13 @@ export const libraryRoutes: Route[] = [
   {
     path: '',
     loadComponent: () =>
-      import('./library.component').then((m) => m.LibraryComponent)
+      import('./library.component').then((m) => m.LibraryComponent),
+    children: [
+      {
+        path: 'screenshots',
+        loadComponent: () =>
+          import('@ever-co/shared-components').then((m) => m.GalleryComponent),
+      },
+    ]
   },
 ];
