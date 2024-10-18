a77eab30a262ef14eb1a43a8e46b5402c6444100
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-18 21:47:20 +0200

    updated library routes and component HTML to add new routes and modify tab navigation

--- libs/web/library/src/lib/lib.routes.ts
+++ libs/web/library/src/lib/lib.routes.ts
@@ -6,11 +6,22 @@ export const libraryRoutes: Route[] = [
     loadComponent: () =>
       import('./library.component').then((m) => m.LibraryComponent),
     children: [
+      { path: '', redirectTo: 'videos', pathMatch: 'full' },
+      {
+        path: 'videos',
+        loadComponent: () =>
+          import('@ever-co/shared-components').then((m) => m.GalleryComponent),
+      },
+      {
+        path: 'archive',
+        loadComponent: () =>
+          import('@ever-co/shared-components').then((m) => m.GalleryComponent),
+      },
       {
         path: 'screenshots',
         loadComponent: () =>
           import('@ever-co/shared-components').then((m) => m.GalleryComponent),
       },
-    ]
+    ],
   },
 ];
