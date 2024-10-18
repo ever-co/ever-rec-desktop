e54218637103ee7690e405a07d863ae851b834ad
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-18 17:14:15 +0200

    fix: layout

--- libs/web/layout/src/lib/layout/layout.component.ts
+++ libs/web/layout/src/lib/layout/layout.component.ts
@@ -1,5 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { breadcrumbActions } from '@ever-co/breadcrumb-data-access';
 import { BreadcrumbComponent } from '@ever-co/breadcrumb-feature';
@@ -12,13 +21,21 @@ import { Store } from '@ngrx/store';
     CommonModule,
     RouterModule,
     BreadcrumbComponent,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatInputModule,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
 export class LayoutComponent implements OnInit {
-  private readonly store = inject(Store);
-
+  constructor(private readonly store: Store) {}
   ngOnInit(): void {
     this.store.dispatch(
       breadcrumbActions.add({ breadcrumb: { label: 'Home', url: '/' } })
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     './apps/ui/src/**/*.{html,ts}',
     './libs/web/**/src/**/*.{html,ts}',
-    './libs/shared/components/**/src/**/*.{html,ts}'
+    './libs/shared/components/**/src/**/*.{html,ts}',
+    './node_modules/@angular/material/**/*.js',
   ],
   theme: {
     extend: {},
