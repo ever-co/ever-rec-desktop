abd5e879f585777cb1d25429c0ae40ad0d8a32fa
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-19 15:42:36 +0200

    "Refactor app.config.ts, update dependencies in package.json and yarn.lock"

--- apps/ui/src/app/app.config.ts
+++ apps/ui/src/app/app.config.ts
@@ -1,10 +1,7 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {
-  provideRouter,
-  withComponentInputBinding,
-  withViewTransitions,
+  provideRouter
 } from '@angular/router';
 import { provideBreadcrumbDataAccess } from '@ever-co/breadcrumb-data-access';
 import { provideConvertVideoDataAccess } from '@ever-co/convert-video-data-access';
@@ -12,22 +9,20 @@ import { provideScreenshotDataAccess } from '@ever-co/screenshot-data-access';
 import { provideSidebarDataAccess } from '@ever-co/sidebar-data-access';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(),
-    provideEffects(),
-    provideStore(),
     provideScreenshotDataAccess(),
     provideBreadcrumbDataAccess(),
     provideConvertVideoDataAccess(),
     provideSidebarDataAccess(),
-    provideRouter(
-      appRoutes,
-      withComponentInputBinding(),
-      withViewTransitions()
-    ),
+    provideStoreDevtools({ logOnly: !isDevMode() }),
+    provideEffects(),
+    provideStore(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
     provideAnimationsAsync(),
   ],
 };
