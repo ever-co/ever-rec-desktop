4bce3cf0ac56cfd4b6fca3d1b7bd9eeb0ed04742
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-20 01:00:05 +0200

    "Updated dashboard component to display dynamic state and screenshot count"

--- libs/web/dashboard/src/lib/dashboard/dashboard.component.ts
+++ libs/web/dashboard/src/lib/dashboard/dashboard.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { selectGenerateVideoState } from '@ever-co/convert-video-data-access';
+import { selectScreenshotState } from '@ever-co/screenshot-data-access';
 import { NoDataComponent } from '@ever-co/shared-components';
+import { Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'lib-dashboard',
@@ -10,4 +14,15 @@ import { NoDataComponent } from '@ever-co/shared-components';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit{
+  public capturing$!: Observable<boolean>;
+  public generating$!: Observable<boolean>;
+  public screenshotCount$!: Observable<number>;
+
+  constructor(private readonly store: Store) {}
+  ngOnInit(): void {
+    this.capturing$ = this.store.select(selectScreenshotState).pipe(map(state => state.capturing));
+    this.screenshotCount$ = this.store.select(selectScreenshotState).pipe(map(state => state.count));
+    this.generating$ = this.store.select(selectGenerateVideoState).pipe(map(state => state.generating));
+  }
+}
