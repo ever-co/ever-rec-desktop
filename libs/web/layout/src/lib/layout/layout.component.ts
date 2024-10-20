f7551edf3b94324cb05984621760e119c0fed766
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-20 18:28:03 +0200

    Refactor layout component to use Material sidenav and add mobile view support

--- libs/web/layout/src/lib/layout/layout.component.ts
+++ libs/web/layout/src/lib/layout/layout.component.ts
@@ -1,5 +1,6 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -16,6 +17,7 @@ import { StartComponent, StopComponent } from '@ever-co/screenshot-feature';
 import { SidebarComponent } from '@ever-co/sidebar-feature';
 import { SearchComponent } from '@ever-co/web-search';
 import { Store } from '@ngrx/store';
+import { Subject, debounceTime, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'lib-layout',
@@ -41,13 +43,35 @@ import { Store } from '@ngrx/store';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent implements OnInit{
-  constructor(private readonly store: Store) {}
-  ngOnInit(): void {
+export class LayoutComponent implements OnInit, OnDestroy{
+  private readonly destroy$ = new Subject<void>();
+  public isExpanded = false;
+  public isMobileView = false;
+  constructor(private readonly store: Store, private readonly breakpointObserver: BreakpointObserver) {}
+  public ngOnInit(): void {
     this.store.dispatch(
       breadcrumbActions.set({
         breadcrumbs: [{ label: 'Dashboard', url: 'dashboard' }],
       })
     );
+    this.breakpointObserver
+      .observe(['(max-width: 767px)'])
+      .pipe(debounceTime(100), takeUntil(this.destroy$))
+      .subscribe((result) => {
+        this.isMobileView = result.matches;
+        if (this.isExpanded && !this.isMobileView) {
+          this.isExpanded = false;
+        }
+      });
+  }
+
+  public toggleSidenav() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  public ngOnDestroy(): void {
+    // Complete the destroy$ observable to unsubscribe all subscriptions
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
