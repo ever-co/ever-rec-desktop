fa3b117b5148b3163f90fce261493aaec5db2d61
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-21 20:31:55 +0200

    "Updated video component: added video player controls, toggled play/pause button, and modified HTML, SCSS, and TS files accordingly."

--- libs/shared/components/src/lib/ui/video/video.component.ts
+++ libs/shared/components/src/lib/ui/video/video.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { UtcToLocalTimePipe } from '@ever-co/shared-service';
@@ -8,11 +9,41 @@ import { IVideo } from '@ever-co/shared-utils';
 @Component({
   selector: 'lib-video',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatIconModule, UtcToLocalTimePipe],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    UtcToLocalTimePipe,
+  ],
   templateUrl: './video.component.html',
   styleUrl: './video.component.scss',
 })
 export class VideoComponent {
+  @ViewChild('videoPlayer', { static: false })
+  public videoPlayer!: ElementRef<HTMLVideoElement>;
+
   @Input()
-  public video!: IVideo
+  public controls = false;
+
+  @Input()
+  public video!: IVideo;
+
+  public isPlayed = false;
+  public async togglePlayPause(): Promise<void>{
+    const player = this.videoPlayer?.nativeElement;
+
+    if (!player) {
+      console.error('Video player is not available');
+      return;
+    }
+
+    if (this.isPlayed) {
+      player.pause();
+    } else {
+       await player.play();
+    }
+
+    this.isPlayed = !this.isPlayed;
+  }
 }
