ee4c9294236a1da3a808b9503a4e695543bd03c7
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-23 12:20:54 +0200

    "Refactor SearchOverlayComponent: update HTML, SCSS, and TS files to modify search overlay layout and behavior"

--- libs/web/search/src/lib/search-overlay/search-overlay.component.ts
+++ libs/web/search/src/lib/search-overlay/search-overlay.component.ts
@@ -4,14 +4,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { Router } from '@angular/router';
 import {
   screenshotActions,
   selectScreenshotState,
 } from '@ever-co/screenshot-data-access';
 import { NoDataComponent } from '@ever-co/shared-components';
 import { InfiniteScrollDirective } from '@ever-co/shared-service';
+import { IScreenshot } from '@ever-co/shared-utils';
 import { Store } from '@ngrx/store';
-import { tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { SearchService } from '../search/search.service';
 
 @Component({
@@ -36,18 +38,28 @@ export class SearchOverlayComponent implements OnInit {
   private currentPage = 1;
   private hasNext = false;
 
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store, private readonly router: Router) {}
   ngOnInit(): void {
     this.store
       .select(selectScreenshotState)
       .pipe(
         tap((state) => {
-          console.log(state)
+          console.log(state);
           this.hasNext = state.search.hasNext;
           this.searchService.search.set(state.search.screenshots);
         })
       )
       .subscribe();
+  }
+
+  public async showResult({ id }: IScreenshot) {
+    await this.router.navigate(['/', 'library', 'screenshots', id]);
+  }
+
+  public get count$(): Observable<number> {
+    return this.store
+      .select(selectScreenshotState)
+      .pipe(map((state) => state.search.count));
   }
 
   public moreResult(): void {
