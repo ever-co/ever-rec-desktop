cce8a8b24a4215f49cdbf5ccd2ddf7ff091eadab
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-28 14:12:29 +0200

    updated TimelineComponent: added ProgressComponent to imports, changed isAvailable$ selector, sorted screenshots by createdAt, and modified mergeIcons logic

--- libs/web/convert-video/feature/src/lib/screenshot-timeline/timeline.component.ts
+++ libs/web/convert-video/feature/src/lib/screenshot-timeline/timeline.component.ts
@@ -7,9 +7,10 @@ import {
 } from '@ever-co/convert-video-data-access';
 import { selectScreenshotState } from '@ever-co/screenshot-data-access';
 import { NoDataComponent } from '@ever-co/shared-components';
-import { IScreenshot } from '@ever-co/shared-utils';
+import { IScreenshot, moment } from '@ever-co/shared-utils';
 import { Store } from '@ngrx/store';
 import { map, Observable, Subject, takeUntil } from 'rxjs';
+import { ProgressComponent } from '../progress/progress.component';
 import { VideoComponent } from '../video/video.component';
 
 type AggregatedScreenshot = IScreenshot & { xTimeIcon: number; width: number };
@@ -17,7 +18,13 @@ type AggregatedScreenshot = IScreenshot & { xTimeIcon: number; width: number };
 @Component({
   selector: 'lib-timeline',
   standalone: true,
-  imports: [CommonModule, VideoComponent, NoDataComponent, MatTooltipModule],
+  imports: [
+    CommonModule,
+    VideoComponent,
+    NoDataComponent,
+    MatTooltipModule,
+    ProgressComponent,
+  ],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss',
 })
@@ -35,13 +42,15 @@ export class TimelineComponent implements OnInit, OnDestroy {
       map((state) => state.capturing || state.loading),
       takeUntil(this.destroy$)
     );
-    this.isAvailable$ = this.store.select(selectScreenshotState).pipe(
-      map((state) => state.count > 0),
+    this.isAvailable$ = this.store.select(selectGenerateVideoState).pipe(
+      map(({ video }) => !!video.pathname),
       takeUntil(this.destroy$)
     );
     this.screenshots$ = this.store.select(selectGenerateVideoState).pipe(
       map(({ video }) => {
-        const screenshots = video?.screenshots || [];
+        const screenshots = [...(video?.screenshots || [])].sort((a, b) =>
+          moment(a.createdAt).diff(moment(b.createdAt))
+        );
         return this.mergeIcons(screenshots);
       }),
       takeUntil(this.destroy$)
@@ -52,27 +61,27 @@ export class TimelineComponent implements OnInit, OnDestroy {
     const size = screenshots.length;
     if (!size) return [];
 
-    // const result: AggregatedScreenshot[] = [];
-    // let current: AggregatedScreenshot = {
-    //   ...screenshots[0],
-    //   xTimeIcon: 1,
-    //   width: this.clamp(1920 / size),
-    // };
+    const result: AggregatedScreenshot[] = [];
+    let current: AggregatedScreenshot = {
+      ...screenshots[0],
+      xTimeIcon: 1,
+      width: this.clamp(1920 / size),
+    };
 
-    // for (let i = 1; i < size; i++) {
-    //   const screenshot = screenshots[i];
-    //   if (current.metadata?.name === screenshot.metadata?.name) {
-    //     current.xTimeIcon++;
-    //   } else {
-    //     result.push(current);
-    //     current = {
-    //       ...screenshot,
-    //       xTimeIcon: 1,
-    //       width: this.clamp(1920 / size),
-    //     };
-    //   }
-    // }
-    // result.push(current);
+    for (let i = 1; i < size; i++) {
+      const screenshot = screenshots[i];
+      if (current.metadata?.name === screenshot.metadata?.name) {
+        current.xTimeIcon++;
+      } else {
+        result.push(current);
+        current = {
+          ...screenshot,
+          xTimeIcon: 1,
+          width: this.clamp(1920 / size),
+        };
+      }
+    }
+    result.push(current);
 
     return screenshots.map((screenshot) => ({
       ...screenshot,
@@ -81,7 +90,14 @@ export class TimelineComponent implements OnInit, OnDestroy {
     }));
   }
 
-  private clamp(value: number, min = 64, max = 1920): number {
+  public get generating$(): Observable<boolean> {
+    return this.store.select(selectGenerateVideoState).pipe(
+      map((state) => state.generating),
+      takeUntil(this.destroy$)
+    );
+  }
+
+  private clamp(value: number, min = 48, max = 1920): number {
     return Math.max(min, Math.min(max, value));
   }
 
