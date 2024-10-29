68e7a7c1a2bbd9953826c847d61da313b8efec75
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-29 06:57:03 +0200

    added dispatch of load action in initializeRetentionFactory

--- libs/web/settings/data-access/src/lib/factory/initialize-retention.factory.ts
+++ libs/web/settings/data-access/src/lib/factory/initialize-retention.factory.ts
@@ -1,12 +1,14 @@
 import { Store } from '@ngrx/store';
 import { distinctUntilChanged, map, tap } from 'rxjs';
 import { StorageElectronService } from '../+state/services/storage-electron.service';
+import { settingStorageActions } from '../+state/storage-setting/storage-setting.actions';
 import { selectSettingStorageState } from '../+state/storage-setting/storage-setting.selectors';
 
 export function initializeRetentionFactory(
   store: Store,
   service: StorageElectronService
 ): () => void {
+  store.dispatch(settingStorageActions.load());
   return () => {
     store
       .select(selectSettingStorageState)
