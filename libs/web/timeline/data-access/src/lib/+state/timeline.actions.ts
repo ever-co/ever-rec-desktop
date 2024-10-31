8feb3adc9af22b548f6115a3c313dd0a105fb9be
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-31 19:19:31 +0200

    refactor timeline interface: add new properties and interfaces, remove old ones, and reorganize structure

--- libs/web/timeline/data-access/src/lib/+state/timeline.actions.ts
+++ libs/web/timeline/data-access/src/lib/+state/timeline.actions.ts
@@ -1,10 +1,16 @@
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { ITimelineItem } from '@ever-co/shared-utils';
+import { createActionGroup, props } from '@ngrx/store';
 
 export const TimelineActions = createActionGroup({
   source: 'Timeline',
   events: {
-    'Timeline Timelines': emptyProps(),
-    
-    
-  }
+    'Timeline Add Item': props<{ item: ITimelineItem }>(),
+    'Timeline Remove Item': props<{ item: ITimelineItem }>(),
+    'Timeline Update Item': props<{ item: ITimelineItem }>(),
+
+    'Timeline Reorder Items': props<{ items: ITimelineItem[] }>(),
+
+    'Timeline Move Cursor': props<{ position: number }>(),
+    'Timeline Move Cursor To Item': props<{ item: ITimelineItem }>(),
+  },
 });
