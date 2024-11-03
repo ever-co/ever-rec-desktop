fadd3b958d0172719487cce7301292b87c2064bc
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-11-03 11:49:23 +0200

    remove console.log statement in setIconCounter method.

--- libs/web/notification/data-access/src/lib/services/notification-electron.service.ts
+++ libs/web/notification/data-access/src/lib/services/notification-electron.service.ts
@@ -9,7 +9,6 @@ export class NotificationElectronService {
   constructor(private readonly electronService: ElectronService) {}
 
   public setIconCounter(count: number) {
-    console.log(count)
     this.electronService.send(Channel.UPDATE_ICON_BADGE, count);
   }
 }
