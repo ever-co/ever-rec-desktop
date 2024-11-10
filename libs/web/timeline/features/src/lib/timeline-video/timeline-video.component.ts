c59779cc09a01fc6ece7b50419d1579e5cdde4d0
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-11-10 16:47:21 +0200

    feat: add video timeline player component

---
+++ libs/web/timeline/features/src/lib/timeline-video/timeline-video.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-timeline-video',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './timeline-video.component.html',
+  styleUrl: './timeline-video.component.scss',
+})
+export class TimelineVideoComponent {}
