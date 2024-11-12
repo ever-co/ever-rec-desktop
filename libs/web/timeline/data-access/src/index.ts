8cae3c0c2b31d76d323e2c88101a660dd37e058a
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-11-12 22:17:10 +0200

    remove export of timeline-facade.service from index.ts

--- libs/web/timeline/data-access/src/index.ts
+++ libs/web/timeline/data-access/src/index.ts
@@ -2,7 +2,6 @@ export * from './lib/+state/timeline.actions';
 export * from './lib/+state/timeline.effects';
 export * from './lib/+state/timeline.reducer';
 export * from './lib/+state/timeline.selectors';
-export * from './lib/services/timeline-facade.service';
 export * from './lib/timeline-data.module';
 export * from './test-setup';
 
