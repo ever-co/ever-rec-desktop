29b2c34ac2df1bed805dcee17ac243b0365540d0
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-11-17 08:08:36 +0200

    feat: add jsdoc

--- libs/electron/event/src/lib/power-manager.event.ts
+++ libs/electron/event/src/lib/power-manager.event.ts
@@ -4,21 +4,30 @@ import {
   TimerScheduler,
   UnlockScreenHandler
 } from '@ever-co/electron-utils';
+/**
+ * Registers the power manager event handlers.
+ * This function registers the power manager event handlers on the TimerScheduler's start event.
+ * The event handlers are registered with the PowerEventManager.
+ * When the TimerScheduler is stopped, the PowerEventManager is stopped as well.
+ */
 export function powerManagerHandlerEvents(): void {
   const timerScheduler = TimerScheduler.getInstance();
   const powerEventManager = PowerEventManager.getInstance();
 
   timerScheduler.onStart(() => {
+    // Register the unlock-screen and lock-screen event handlers.
     powerEventManager.registerHandler(
       'unlock-screen',
       new UnlockScreenHandler()
     );
     powerEventManager.registerHandler('lock-screen', new LockScreenHandler());
 
+    // Start the PowerEventManager.
     powerEventManager.start();
   });
 
   timerScheduler.onStop(() => {
+    // Stop the PowerEventManager when the TimerScheduler is stopped.
     powerEventManager.stop();
   });
 }
