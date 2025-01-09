3df8820ba4f8c4be3ee733349b62f5d02a7a1fd2
adkif <akifungo@gmail.com>
2025-01-09 15:29:53 +0200

    feat: logs the result of the upload process
    Adds a console log to display the result received from the electron upload process. This helps with debugging and monitoring the upload status.

--- libs/web/upload/data-access/src/lib/services/video-upload.service.ts
+++ libs/web/upload/data-access/src/lib/services/video-upload.service.ts
@@ -46,6 +46,7 @@ export class VideoUploadService {
     return new Observable<void>((observer) => {
       this.electronService.removeAllListeners(Channel.UPLOAD_DONE);
       this.electronService.on(Channel.UPLOAD_DONE, (_, result) => {
+        console.log('result', result);
         observer.next(result);
         observer.complete();
       });
