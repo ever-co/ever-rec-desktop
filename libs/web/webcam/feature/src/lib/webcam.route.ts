c9caadfc27518caab219ce964173dfdb66f03d52
adkif <akifungo@gmail.com>
2025-04-12 13:14:52 +0200

    feat(webcam): update webcam route to incorporate new preview and control components

--- libs/web/webcam/feature/src/lib/webcam.route.ts
+++ libs/web/webcam/feature/src/lib/webcam.route.ts
@@ -6,6 +6,15 @@ export const settingRoutes: Route[] = [
     path: '',
     loadComponent: () =>
       import('./setting/setting.component').then((m) => m.SettingComponent),
+    children: [
+      { path: '', redirectTo: 'preview', pathMatch: 'full' },
+      {
+        path: 'preview',
+        loadComponent: () =>
+          import('./preview/preview.component').then((m) => m.PreviewComponent),
+      },
+      { path: '**', redirectTo: '' },
+    ],
   },
   { path: '**', redirectTo: '' },
 ];
