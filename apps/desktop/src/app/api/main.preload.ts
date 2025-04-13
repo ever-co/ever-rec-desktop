115637683b97bfab1a7f28bc049c0f6a6361e8da
adkif <akifungo@gmail.com>
2025-04-13 18:44:43 +0200

    refactor(desktop-api): use direct Channel type import in preload
    Remove the unnecessary alias 'ChEnum' for the imported 'Channel' type
    from '@ever-co/shared-utils'.
    Use the original type name 'Channel' directly throughout the main
    preload script for clarity and consistency.

--- apps/desktop/src/app/api/main.preload.ts
+++ apps/desktop/src/app/api/main.preload.ts
@@ -1,49 +1,49 @@
-import { Channel as ChEnum } from '@ever-co/shared-utils';
+import { Channel } from '@ever-co/shared-utils';
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
 type IpcCallback = (event: IpcRendererEvent, ...args: any[]) => void;
 
-const validChannels = Object.values(ChEnum); // Replace with your actual channels
+const validChannels = Object.values(Channel); // Replace with your actual channels
 
 ipcRenderer.setMaxListeners(0);
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  invoke: (channel: ChEnum, data?: any): Promise<any> => {
+  invoke: (channel: Channel, data?: any): Promise<any> => {
     if (validChannels.includes(channel)) {
       return ipcRenderer.invoke(channel, data);
     } else {
       throw new Error(`Invalid channel: ${channel}`);
     }
   },
-  on: (channel: ChEnum, callback: IpcCallback): void => {
+  on: (channel: Channel, callback: IpcCallback): void => {
     if (validChannels.includes(channel)) {
       ipcRenderer.on(channel, callback);
     } else {
       console.error(`Invalid channel: ${channel}`);
     }
   },
-  once: (channel: ChEnum, callback: IpcCallback): void => {
+  once: (channel: Channel, callback: IpcCallback): void => {
     if (validChannels.includes(channel)) {
       ipcRenderer.once(channel, callback);
     } else {
       console.error(`Invalid channel: ${channel}`);
     }
   },
-  send: (channel: ChEnum, data?: any): void => {
+  send: (channel: Channel, data?: any): void => {
     if (validChannels.includes(channel)) {
       ipcRenderer.send(channel, data);
     } else {
       console.error(`Invalid channel: ${channel}`);
     }
   },
-  removeAllListeners: (channel: ChEnum): void => {
+  removeAllListeners: (channel: Channel): void => {
     if (validChannels.includes(channel)) {
       ipcRenderer.removeAllListeners(channel);
     } else {
       console.error(`Invalid channel: ${channel}`);
     }
   },
-  removeListener: (channel: ChEnum, callback: IpcCallback): void => {
+  removeListener: (channel: Channel, callback: IpcCallback): void => {
     if (validChannels.includes(channel)) {
       ipcRenderer.removeListener(channel, callback);
     } else {
