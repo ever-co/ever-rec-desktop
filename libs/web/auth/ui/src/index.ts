0762b03fcafb4ba8be1908c91556b45dff8d6cad
adkif <akifungo@gmail.com>
2025-07-14 12:56:29 +0200

    feat(auth-ui): implement sign-up form component
    Add a new SignUpFormComponent with a reactive form for user registration. The component includes fields for full name, email, password, and confirm password, along with validation and error handling. It also features password visibility toggles and integrates Angular Material components for improved user experience. Update the export path for the sign-up form component to reflect the new naming convention.
    
    ERP-16

--- libs/web/auth/ui/src/index.ts
+++ libs/web/auth/ui/src/index.ts
@@ -1,3 +1,3 @@
 export * from './lib/auth-container/auth-container.component';
 export * from './lib/login-form/login-form.component';
-export * from './lib/signup-form/signup-form.component';
+export * from './lib/sign-up-form/sign-up-form.component';
